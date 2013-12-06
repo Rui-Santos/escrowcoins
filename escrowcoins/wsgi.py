9a898bd721866171f9966a6322e03631b8514119
Madra David <madradavid@gmail.com>
2013-12-06 14:11:18 +0300

    modified git

--- escrowcoins/wsgi.py
+++ escrowcoins/wsgi.py
@@ -14,11 +14,13 @@ framework.
 
 """
 import os
+import sys
 
 # We defer to a DJANGO_SETTINGS_MODULE already in the environment. This breaks
 # if running multiple sites in the same mod_wsgi process. To fix this, use
 # mod_wsgi daemon mode with each site in its own daemon process, or use
 # os.environ["DJANGO_SETTINGS_MODULE"] = "escrowcoins.settings"
+sys.path.append('/media/data/python/websites/escrowcoins')
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "escrowcoins.settings")
 
 # This application object is used by any WSGI server configured to use this
