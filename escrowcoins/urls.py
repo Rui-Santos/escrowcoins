2fa965c95d6f077c5bd99a59896dea43969563a0
Madra David <madradavid@gmail.com>
2013-12-08 11:49:15 +0300

    added userena for user auth

--- escrowcoins/urls.py
+++ escrowcoins/urls.py
@@ -1,17 +1,35 @@
 from django.conf.urls import patterns, include, url
-
+from escrowcoins.views import landingpage, testingpage
+from django.conf import settings
 # Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'escrowcoins.views.home', name='home'),
-    # url(r'^escrowcoins/', include('escrowcoins.foo.urls')),
+                      url(r'^$',landingpage),
+                      (r'^accounts/', include('userena.urls')),
+                       # Examples:
+                       # url(r'^$', 'escrowcoins.views.home', name='home'),
+                       # url(r'^escrowcoins/',
+                       # include('escrowcoins.foo.urls')),
 
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+                       # Uncomment the admin/doc line below to enable admin documentation:
+                       # url(r'^admin/doc/',
+                       # include('django.contrib.admindocs.urls')),
 
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
-)
+                       # Uncomment the next line to enable the admin:
+                       url(r'^admin/', include(admin.site.urls)),
+                       )
+
+if settings.LOCALHOST :
+    urlpatterns += patterns('',
+             (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT, 'show_indexes':True}),
+         )
+
+    urlpatterns += patterns('',
+            (r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT, 'show_indexes':True}),
+        )
+    #url for testing
+    urlpatterns += patterns('',
+            (r'^test',testingpage),
+        )
